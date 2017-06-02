333f432d846c7e979d6631834fd0e9f0a8a2cc0e
Vitkovsky <vzashibis@gmail.com>
2017-06-03 01:58:21 +0300

    add PWM

--- SUB.ino
+++ SUB.ino
@@ -1,16 +1,21 @@
 
 #include <iarduino_IR_RX.h>
 
-#define IS_DEBUG 0
+#define IS_DEBUG 1
 
 #define SUB_BUTTON 2
 #define SUB_RELEY_1 3
-#define SUB_RELEY_2 4
-#define SUB_IR 12
+#define SUB_IR 11
 #define SUB_IR_ON 0xFF6897
 #define SUB_IR_OFF 0xFF9867
+#define FUN_IR_UP 0xFF629D
+#define FUN_IR_DOWN 0xFFA857
 
-#define SUB_RUN_LOOP_DELAY 20
+#define FUN_PWM 5
+#define FUN_ERROR 4
+#define FUN_ERROR_POINTS 200
+
+#define SUB_RUN_LOOP_DELAY 10
 #define SUB_DELAY_POINT_ON 30
 #define SUB_DELAY_POINT_OVERALL 100
 
@@ -19,6 +24,10 @@ bool _isOn;
 bool _button;
 bool _isOffButton;
 int _counter = SUB_DELAY_POINT_ON;
+int _counter_Fun_Error;
+
+int _fun_pwm = 255;//0 - 255
+bool _fun_Error;
 
 iarduino_IR_RX IR(SUB_IR);
 
@@ -27,7 +36,9 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(SUB_BUTTON, INPUT_PULLUP);
   pinMode(SUB_RELEY_1, OUTPUT);
-  pinMode(SUB_RELEY_2, OUTPUT);
+
+  pinMode(FUN_PWM, OUTPUT);
+  pinMode(FUN_ERROR, INPUT_PULLUP);
 
   if (IS_DEBUG) {
     Serial.begin(9600);
@@ -54,6 +65,10 @@ void loop() {
     checkIR();
   }
 
+  if (_isOn) {
+    checkPWMError();
+  }
+
   _counter += _counter == SUB_DELAY_POINT_OVERALL? - SUB_DELAY_POINT_OVERALL + SUB_DELAY_POINT_ON : 1;
   
   delay(SUB_RUN_LOOP_DELAY);
@@ -66,6 +81,11 @@ void checkIR() {
     if (IS_DEBUG) {
       Serial.println(IR.data, HEX);
       Serial.println(IR.length);
+      if (IR.data == FUN_IR_UP) {
+        setPWM(_fun_pwm + 10);
+      }else if (IR.data == FUN_IR_DOWN) {
+        setPWM(_fun_pwm - 10);
+      }
     }
     if (IR.data == SUB_IR_ON) {
       setOn(true);
@@ -86,8 +106,51 @@ void setOn(bool isOn) {
     }
     digitalWrite(LED_BUILTIN, _isOn);
     digitalWrite(SUB_RELEY_1, _isOn);
-    digitalWrite(SUB_RELEY_2, _isOn);
+    if (!_isOn) {
+      setFunError(false);
+    }
   }
   _counter = 0;
   
 }
+
+void setPWM(int fun_pwm) {
+  
+  if (_fun_pwm != fun_pwm) {
+    _fun_pwm = fun_pwm;
+    analogWrite(FUN_PWM, _fun_pwm);
+    if (IS_DEBUG) {
+      Serial.print("_fun_pwm: ");
+      Serial.println(_fun_pwm);
+    }
+  }
+  
+}
+
+void setFunError(bool fun_Error) {
+  if (_fun_Error != fun_Error) {
+    if (IS_DEBUG) {
+      Serial.print("fun_Error: ");
+      Serial.println(fun_Error);
+    }
+  }
+  _fun_Error = fun_Error;
+}
+
+void checkPWMError() {
+  
+  static int funRead = 0;
+  funRead += !digitalRead(FUN_ERROR);
+  if (_counter_Fun_Error == FUN_ERROR_POINTS * SUB_RUN_LOOP_DELAY) {
+
+    setFunError(!funRead);
+    
+    _counter_Fun_Error = 0;
+    funRead = 0;
+  }
+
+  _counter_Fun_Error += SUB_RUN_LOOP_DELAY;
+
+}
+
+
